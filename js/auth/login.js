30c8627a4dee0a255b375ed52d6e19492e193ba4
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-11-07 16:22:16 -0300

    Criar função para pegar credenciais

---
+++ js/auth/login.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form");
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const emailValue = document.getElementById("email").value;
+    const passwordValue = document.getElementById("password").value;
+
+})
