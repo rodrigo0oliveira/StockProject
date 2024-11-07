8e8704a5082dfb127d23e4b1df68c1ed25ded471
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-11-07 15:48:04 -0300

    Adicionar backend e criar entidades de user e roles

---
+++ backend/src/main/java/br/com/estoque/backend/entities/Roles.java
@@ -0,0 +1,42 @@
+package br.com.estoque.backend.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "tb_roles")
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Roles implements GrantedAuthority{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "role_id")
+    private String id;
+
+    private String name;
+
+    public Roles(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    @Override
+	public String toString() {
+        return "{\"id\":\"" + id + "\", \"name\":\"" + name + "\"}";
+    }
+}
---
+++ backend/src/main/java/br/com/estoque/backend/entities/User.java
@@ -0,0 +1,74 @@
+package br.com.estoque.backend.entities;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private String id;
+
+    @Email
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name="role_id"))
+    private Collection<Roles> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getUsername();
+    }
+
+    @Override
+	public String toString() {
+		return "{" +
+                "\"id\":\"" + id+ "\"," +
+                "\"email\":\"" + email + "\"," +
+                "\"password\":\"" + password + "\"," +
+                "\"roles\":" + roles.toString() +
+                "}";
+	}
+
+}
