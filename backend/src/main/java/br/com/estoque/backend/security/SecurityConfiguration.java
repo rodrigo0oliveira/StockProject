6692ec493d77ac844454298b66a1ed142f014ab8
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-11-09 12:56:59 -0300

    Mudanças no csrf e habilitando endpoint

--- backend/src/main/java/br/com/estoque/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/br/com/estoque/backend/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,8 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -39,22 +42,27 @@ public class SecurityConfiguration {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    public static final String [] FREE_ENDPOINTS ={
+        "/auth/register"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
-        .csrf(csrf-> csrf.disable())
+        .csrf().disable()
         .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(FREE_ENDPOINTS).permitAll()
         .requestMatchers(PathRequest.toH2Console()).permitAll()
         .requestMatchers("/h2-console/**").permitAll()
         .anyRequest().permitAll())
         .headers(headers -> headers
                 .frameOptions().disable() 
             )
-            .csrf(csrf-> csrf.ignoringRequestMatchers("/h2-console/**"))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(new JwtFilter(tokenProvider),UsernamePasswordAuthenticationFilter.class)
-        .httpBasic(httpBasic-> httpBasic.disable())
+        .httpBasic(httpBasic-> httpBasic.authenticationEntryPoint((AuthenticationEntryPoint)
+        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
         .exceptionHandling(exceptionHandling -> exceptionHandling.accessDeniedHandler(new AccessDeniedHandlerImpl()));
 
 
