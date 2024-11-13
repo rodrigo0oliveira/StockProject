de4f8d7a2be41da1a4a37a78b7c1ec684fa97b9d
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-11-13 15:02:41 -0300

    Criar funcionalidades e realizar requisições de login

--- frontend/js/auth/login.js
+++ frontend/js/auth/login.js
@@ -1,9 +1,50 @@
+import LoginDto from "../dtos/loginDto.js"
+
 const form = document.getElementById("form");
 
-form.addEventListener('submit',function(event){
+form.addEventListener('submit', async function(event){
     event.preventDefault();
 
     const emailValue = document.getElementById("email").value;
     const passwordValue = document.getElementById("password").value;
 
+    const loginDto = new LoginDto(emailValue,passwordValue);
+
+    try{
+        const response = await sendRequestLogin(loginDto);
+        responseMessageLogin(response.status);
+
+    }catch(error){
+        return error;
+    }
+
 })
+
+async function sendRequestLogin(user) {
+    try{
+        const response = await axios.post('http://localhost:8080/auth/login',{
+            email : user.email,
+            password: user.password
+        },{
+            validateStatus:(status)=>{
+                return status>=200 && status <500;
+            }
+        });
+        return response;
+    }
+    catch(errror){
+        return errror;
+    }
+}
+
+function responseMessageLogin(response){
+    const spanCredentialError = document.getElementById("credential-error");
+    if(response==200){
+        spanCredentialError.textContent = "Credenciais válidas";
+        spanCredentialError.style.color = "green";
+        spanCredentialError.style.display = "block";
+    }
+    else{
+        spanCredentialError.style.display = "block";
+    }
+}
